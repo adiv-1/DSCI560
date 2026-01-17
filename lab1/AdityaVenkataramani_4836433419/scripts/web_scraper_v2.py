4bee099a7bca97e9170202652cbdf9f7ea622216
adiv-1 <aditya.venkat@alumni.emory.edu>
2026-01-17 09:58:12 +0000

    adjustments made to scripts and final data scraping/filtering logic

--- lab1/AdityaVenkataramani_4836433419/scripts/web_scraper_v2.py
+++ lab1/AdityaVenkataramani_4836433419/scripts/web_scraper_v2.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 url = "https://www.cnbc.com/world/?region=world"
@@ -14,38 +13,23 @@ os.makedirs("../data/raw_data_v2", exist_ok=True)
 os.makedirs("../data/processed_data_v2", exist_ok=True)
 
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+service = Service("/usr/bin/chromedriver")
+browser = webdriver.Chrome(service=service, options=options)
 
 browser.get(url)
-
 time.sleep(3)
 
-try:
-    card = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "[class*='MarketCard-container']"))
-    )
-    time.sleep(2)
-except:
-    pass
+WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class*='MarketCard-container']")))
 
-try:
-    news_list = browser.find_element(By.CSS_SELECTOR, "[class*='LatestNews-list']")
-    for i in range(10):
-        browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", news_list)
-        time.sleep(2)
-except:
-    pass
-
-html = browser.page_source
+html_news = browser.page_source
 browser.quit()
 
-file = open("../data/raw_data_v2/web_data.html", "w", encoding="utf-8")
-file.write(html)
-file.close()
+with  open("../data/raw_data_v2/web_data.html", "w", encoding="utf-8") as f:
+    f.write(html_news)
 
 file = open("../data/raw_data_v2/web_data.html", "r", encoding="utf-8")
 lines = file.readlines()
